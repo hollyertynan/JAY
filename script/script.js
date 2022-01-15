39a9b6966f34375373822d10f0e88a42cf950bfe
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2022-01-15 13:49:11 -0500

    gallery updates

---
+++ script/script.js
@@ -0,0 +1,6 @@
+function onNonfiction() {
+    $("#gallery").empty();
+    $("#gallery").html(
+        "<div class=\"newRow\"><div class=\"column\"><img src=\"assets/tour/nonfiction/img_3.png\"><img src=\"assets/tour/nonfiction/img_2.png\"><img src=\"assets/tour/nonfiction/img_1.png\"></div><div class=\"column\"><img src=\"assets/tour/nonfiction/img_4.png\"><img src=\"assets/tour/nonfiction/img_8.png\"><img src=\"assets/tour/nonfiction/img_6.png\"></div><div class=\"column\"><img src=\"assets/tour/nonfiction/img_9.png\"><img src=\"assets/tour/nonfiction/img_5.png\"><img src=\"assets/tour/nonfiction/img_11.jpeg\"></div><div class=\"column\"><img src=\"assets/tour/nonfiction/img_10.png\"><img src=\"assets/tour/nonfiction/img_7.png\"><img src=\"assets/tour/nonfiction/7_30_21_1.jpeg\"></div>"
+    );
+}
