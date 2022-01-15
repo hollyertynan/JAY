d6b76da6f6ead4de8df05fd4b7e1716246192ae2
Tynan Hollyer <44280621+hollyertynan@users.noreply.github.com>
2022-01-15 13:54:48 -0500

    gallery fixes

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 function onNonfiction() {
     $("#gallery").empty();
     $("#gallery").html(
-        "<div class=\"newRow\"><div class=\"column\"><img src=\"assets/tour/nonfiction/img_3.png\"><img src=\"assets/tour/nonfiction/img_2.png\"><img src=\"assets/tour/nonfiction/img_1.png\"></div><div class=\"column\"><img src=\"assets/tour/nonfiction/img_4.png\"><img src=\"assets/tour/nonfiction/img_8.png\"><img src=\"assets/tour/nonfiction/img_6.png\"></div><div class=\"column\"><img src=\"assets/tour/nonfiction/img_9.png\"><img src=\"assets/tour/nonfiction/img_5.png\"><img src=\"assets/tour/nonfiction/img_11.jpeg\"></div><div class=\"column\"><img src=\"assets/tour/nonfiction/img_10.png\"><img src=\"assets/tour/nonfiction/img_7.png\"><img src=\"assets/tour/nonfiction/7_30_21_1.jpeg\"></div>"
+        "<div class=\"newRow\"><div class=\"column\"><img src=\"assets/tour/nonfiction/img_3.PNG\"><img src=\"assets/tour/nonfiction/img_2.PNG\"><img src=\"assets/tour/nonfiction/img_1.PNG\"></div><div class=\"column\"><img src=\"assets/tour/nonfiction/img_4.PNG\"><img src=\"assets/tour/nonfiction/img_8.PNG\"><img src=\"assets/tour/nonfiction/img_6.PNG\"></div><div class=\"column\"><img src=\"assets/tour/nonfiction/img_9.PNG\"><img src=\"assets/tour/nonfiction/img_5.PNG\"><img src=\"assets/tour/nonfiction/img_11.jpeg\"></div><div class=\"column\"><img src=\"assets/tour/nonfiction/img_10.PNG\"><img src=\"assets/tour/nonfiction/img_7.PNG\"><img src=\"assets/tour/nonfiction/7_30_21_1.jpeg\"></div>"
     );
 }
